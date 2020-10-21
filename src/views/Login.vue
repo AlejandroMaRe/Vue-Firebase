283652938c1d8f8886a6b3a41603acbf5d142e75
AlejandroMaRe <alesmay@hotmail.com>
2020-10-21 13:20:06 -0500

    Creacion de vista - login

---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="login">
+        <h3> Sing in </h3>
+        <input type="text" placeholder="Email"><br>
+        <input type="password" placeholder="Password"><br>
+        <button>Connection</button>
+        <p>You don´t have an account ?  You can create one </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'login',
+    data(){
+        return {};
+    },
+    methods:{}
+}
+</script>
+
+<style scoped>
+.login{
+    margin-top: 40px;
+}
+input{
+    margin: 10px;
+    width: 20%;
+    padding: 15px;
+}
+button{
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+}
+p{
+    margin-top: 40px;
+    font-size: 13px;
+}
+p a {
+    text-decoration: underline;
+    cursor: pointer;
+}
+</style>
